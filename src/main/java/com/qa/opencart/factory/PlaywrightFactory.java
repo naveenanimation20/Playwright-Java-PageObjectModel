f810435459c8f3d115b6459a9fc51a913e08269a
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-09-26 12:13:49 +0400

    updated screenshot with base64 path

--- src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -108,11 +108,12 @@ public class PlaywrightFactory {
 
 	public static String takeScreenshot() {
 		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
-
-		getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		//getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
 		
+		byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		String base64Path = Base64.getEncoder().encodeToString(buffer);
 		
-		return path;
+		return base64Path;
 	}
 
 }
